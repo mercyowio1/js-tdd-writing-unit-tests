dc4fb85ee83e49fef43d26457f2137404f0e1eac
Mercy <owinomercy983@gmail.com>
2025-04-28 21:21:31 +0300

    tdd writingtest

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,12 @@
 // Your tests here
+import { pointsForWord } from "../utils";
+
+describe("pointsForWord", () => {
+  it("calculates points for vowels and consonants", () => {
+    expect(pointsForWord("test")).toBe(7);
+  });
+
+  it("handles uppercase and lowercase input", () => {
+    expect(pointsForWord("tEsT")).toBe(7);
+  });
+});
